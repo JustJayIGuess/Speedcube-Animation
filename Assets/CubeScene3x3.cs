fc57e4a0b7e7c269b9f3ec67ce321b7167d517bc
Jay Johnston <40308162+JustJayIGuess@users.noreply.github.com>
2022-03-22 22:25:05 +1000

    Initial commit

---
+++ Assets/CubeScene3x3.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScene3x3 : MonoBehaviour
+{
+	public static class HighlightColors
+	{
+		public static Color White { get; private set; } = new Color(1f, 1f, 1f, 1f);
+		public static Color Red { get; private set; } = new Color(1f, 0.2f, 0.3f, 1f);
+		public static Color Green { get; private set; } = new Color(0.2f, 1f, 0.3f, 1f);
+		public static Color Blue { get; private set; } = new Color(0.4f, 0.4f, 1f, 1f);
+	}
+
+    protected CubeController3x3 cube;
+    public CubeScene3x3 nextScene = null;
+
+    void Start()
+    {
+        cube = FindObjectOfType<CubeController3x3>();
+    }
+
+    protected virtual IEnumerator Scene()
+	{
+        StartCoroutine(nextScene.Scene());
+        yield return null;
+	}
+
+    public void BeginSequence()
+	{
+        StartCoroutine(Scene());
+	}
+}
